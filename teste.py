8c41925ec558834809dc8ad15236e8021823f716
MatheusMenezesss <159057394+MatheusMenezesss@users.noreply.github.com>
2024-07-04 07:46:40 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,14 @@
+import math #importar lib
+nome = int(input("Escreva Sua idade: "))
+str_var = "string qualquer"
+print(nome) #printa na tela
+print(f"mmmmmm {nome} ssssss") #formatacao de strings oque tiver
+print(str_var.upper()) #mostra minha string como maiuscula
+print(abs(-nome)) #modulo do argumento
+print(pow(2,3)) #potencia do 1 elemento elevado ao 2 elemento
+print(max(nome, 12)) #retorna o maior numero entre os argumentos
+print(min(nome, 12)) #retorna o minimo entre os argumentos
+print(round(8.8)) #arredonda o arg
+
+print(math.floor(8.8))#arredonda para baixo
+print(math.ceil(8.8))#arredonda para cima
